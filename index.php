2f914ea7109733e67f244984b53c0dd3b4b25d38
bastienmichaux <bastienmichaux@gmail.com>
2016-03-18 15:38:13 +0100

    file org

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="gl">
 <head>
 	<title>Titre</title>
 	<meta charset="utf-8" />
 	<!-- head.php -->
+		<!-- -->
+		<meta name="Keywords" content="abogado,noia,Castro,avogadas,abogada,abogadas,abogados,Castro2,consultas,online,consultasonline,internet,nuevo,galicia,gallega,santiago,barbanza,coruña,juridico,jurídico,xurídico,bufete,despacho,ley,derecho,dereito,pleito,laboral,civil,penal,administrativo,despido,despidos,salarios,salario,reclamacion,divorcio,herencia,separación,multa,concello,tráfico,incapacidades,incapacidad,pension,donacion,faltas,extranjería,estranxeria,alcoholemia,arraigo,nacionalidad">
+		<meta name="Description" content="abogado noia Castro avogadas abogada abogadas abogados Castro2 consultas online consultas online internet nuevo galicia gallega santiago barbanza coruña juridico jurídico xurídico bufete despacho ley derecho dereito pleito laboral civil penal administrativo despido despidos salarios salario reclamacion divorcio herencia separación multa concello tráfico incapacidades incapacidad pension donacion faltas extranjería estranxeria alcoholemia arraigo nacionalidad">
+		<!-- -->
 		<!-- older smartphones support -->
 		<meta name="handheldfriendly" content="true" />
 		<meta name="mobileoptimized" content="240" />
@@ -15,7 +19,7 @@
 	<!-- /head.php -->
 </head>
 <body>
-	<div id="page_wrap">
+	<div id="page">
 		<div>
 			<div>
 				Article
