6c536753c315c6674e7182f945897ada6b9f529b
bastienmichaux <bastienmichaux@gmail.com>
2016-03-18 15:18:05 +0100

    initial settings
    initializations and stuff

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
 	<title>Titre</title>
 	<meta charset="utf-8" />
+	<!-- head.php -->
+		<!-- older smartphones support -->
+		<meta name="handheldfriendly" content="true" />
+		<meta name="mobileoptimized" content="240" />
+		<!-- /older smartphones support -->
+		<meta name="viewport" content="width=device-width,initial-scale=1,target-densitydpi=device-dpi" />
+		<link rel="stylesheet" lang="text/css" href="css/color.css" />
+		<link rel="stylesheet" lang="text/css" href="css/layout.css" />
+		<link rel="stylesheet" lang="text/css" href="css/text.css" />
+	<!-- /head.php -->
 </head>
 <body>
 	<div id="page_wrap">
