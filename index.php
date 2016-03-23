40ec6644891e40d4871928d1c12d2a7939ff2c64
bastienmichaux <bastienmichaux@gmail.com>
2016-03-23 12:58:12 +0100

    minor mod

--- index.php
+++ index.php
@@ -4,11 +4,15 @@
 	<title>Titre</title>
 	<meta charset="utf-8" />
 	<!-- head.php -->
-
 		<!-- keywords and description -->
+		<!-- TODO SEO -->
 		<meta name="Keywords" content="abogado,noia,Castro,avogadas,abogada,abogadas,abogados,Castro2,consultas,online,consultasonline,internet,nuevo,galicia,gallega,santiago,barbanza,coruña,juridico,jurídico,xurídico,bufete,despacho,ley,derecho,dereito,pleito,laboral,civil,penal,administrativo,despido,despidos,salarios,salario,reclamacion,divorcio,herencia,separación,multa,concello,tráfico,incapacidades,incapacidad,pension,donacion,faltas,extranjería,estranxeria,alcoholemia,arraigo,nacionalidad">
 		<meta name="Description" content="abogado noia Castro avogadas abogada abogadas abogados Castro2 consultas online consultas online internet nuevo galicia gallega santiago barbanza coruña juridico jurídico xurídico bufete despacho ley derecho dereito pleito laboral civil penal administrativo despido despidos salarios salario reclamacion divorcio herencia separación multa concello tráfico incapacidades incapacidad pension donacion faltas extranjería estranxeria alcoholemia arraigo nacionalidad">
 		<!-- /keywords and description -->
+
+		<!-- ico -->
+			<!-- TODO -->
+		<!-- /ico -->
 
 		<!-- older smartphones support -->
 		<meta name="handheldfriendly" content="true" />
@@ -23,16 +27,26 @@
 </head>
 <body>
 	<div id="page">
+		<!-- header -->
 		<?php require "pages/header.php";?>
+		<!-- /header -->
 
-		<?php require "test.php";?>
+		<!-- article -->
+			<?php require "test.php";?>
+			<p>Test 2</p>
 
-		<p>Test 2</p>
+			<!-- Google map -->
+			<iframe width="400" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="3" src="https://maps.google.fr/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=Praza+do+Tapal,+Noia,+Espa%C3%B1a&amp;aq=1&amp;oq=noia&amp;sll=46.75984,1.738281&amp;sspn=14.800841,25.246582&amp;ie=UTF8&amp;hq=&amp;hnear=Praza+do+Tapal,+Noia,+A+Coru%C3%B1a,+Galicia,+Espa%C3%B1a&amp;t=m&amp;z=14&amp;ll=42.782279,-8.888926&amp;output=embed">
+			</iframe>
+			<br />
+			<a href="https://maps.google.fr/maps?f=q&amp;source=embed&amp;hl=es&amp;geocode=&amp;q=Praza+do+Tapal,+Noia,+Espa%C3%B1a&amp;aq=1&amp;oq=noia&amp;sll=46.75984,1.738281&amp;sspn=14.800841,25.246582&amp;ie=UTF8&amp;hq=&amp;hnear=Praza+do+Tapal,+Noia,+A+Coru%C3%B1a,+Galicia,+Espa%C3%B1a&amp;t=m&amp;z=14&amp;ll=42.782279,-8.888926" style="color:#0000FF;text-align:left">
+				Ver mapa máis grande
+			</a>
+			<!-- /Google map -->
+		<!-- /article -->
 
-		<a href="https://maps.google.fr/maps?f=d&source=s_q&hl=es&geocode=%3BCbA1rcQasEDfFUfOjAIdol14_ymbKEgLBSEvDTEDT9E2E8ixyA&q=Praza+do+Tapal,+Noia,+Espa%C3%B1a&aq=1&oq=noia&sll=46.75984,1.738281&sspn=14.800841,25.246582&ie=UTF8&hq=&hnear=Praza+do+Tapal,+Noia,+A+Coru%C3%B1a,+Galicia,+Espa%C3%B1a&t=m&z=14&vpsrc=0&iwloc=A&daddr=Praza+do+Tapal,+15200+Noia,+A+Coru%C3%B1a,+Espa%C3%B1a">
-			<p>Como chegar</p>
-		</a>
-
+		<!-- footer -->
 		<?php require "pages/footer.php";?>
+		<!-- /footer -->
 </body>
 </html>
--- pages/footer.php
+++ pages/footer.php
@@ -1,3 +1,26 @@
 <footer>
   Footer php
+  <div id="wrap-footer">
+    <!-- image -->
+    <!-- /image -->
+
+    <!-- legal advice -->
+    <a target="new" href="avisolegal.html">
+      Aviso legal
+    </a>
+    <!-- /legal advice -->
+
+    <!-- Google map -->
+    <a href="https://maps.google.fr/maps?f=d&source=s_q&hl=es&geocode=%3BCbA1rcQasEDfFUfOjAIdol14_ymbKEgLBSEvDTEDT9E2E8ixyA&q=Praza+do+Tapal,+Noia,+Espa%C3%B1a&aq=1&oq=noia&sll=46.75984,1.738281&sspn=14.800841,25.246582&ie=UTF8&hq=&hnear=Praza+do+Tapal,+Noia,+A+Coru%C3%B1a,+Galicia,+Espa%C3%B1a&t=m&z=14&vpsrc=0&iwloc=A&daddr=Praza+do+Tapal,+15200+Noia,+A+Coru%C3%B1a,+Espa%C3%B1a">
+      <p>Como chegar</p>
+    </a>
+    <!-- /Google map -->
+
+    <!-- facebook button -->
+    <a href="https://www.facebook.com/pages/Castro-%C3%93-Cadrado-Avogadas/1412898085591868?fref=ts">
+      Síguenos
+    </a>
+    <img src="images/face.jpg" />
+    <!-- /facebook button -->
+  </div>
 </footer>
