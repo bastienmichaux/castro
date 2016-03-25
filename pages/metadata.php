3b185b1d2091bcbaf07604119c84ecbeb5b86e37
bastienmichaux <bastienmichaux@gmail.com>
2016-03-25 16:29:26 +0100

    archive

---
+++ pages/metadata.php
@@ -0,0 +1,22 @@
+
+<meta charset="utf-8" />
+
+<!-- keywords and description -->
+<!-- TODO SEO -->
+<meta name="Keywords" content="abogado,noia,Castro,avogadas,abogada,abogadas,abogados,Castro2,consultas,online,consultasonline,internet,nuevo,galicia,gallega,santiago,barbanza,coruña,juridico,jurídico,xurídico,bufete,despacho,ley,derecho,dereito,pleito,laboral,civil,penal,administrativo,despido,despidos,salarios,salario,reclamacion,divorcio,herencia,separación,multa,concello,tráfico,incapacidades,incapacidad,pension,donacion,faltas,extranjería,estranxeria,alcoholemia,arraigo,nacionalidad">
+<meta name="Description" content="abogado noia Castro avogadas abogada abogadas abogados Castro2 consultas online consultas online internet nuevo galicia gallega santiago barbanza coruña juridico jurídico xurídico bufete despacho ley derecho dereito pleito laboral civil penal administrativo despido despidos salarios salario reclamacion divorcio herencia separación multa concello tráfico incapacidades incapacidad pension donacion faltas extranjería estranxeria alcoholemia arraigo nacionalidad">
+<!-- /keywords and description -->
+
+<!-- ico -->
+  <!-- TODO -->
+<!-- /ico -->
+
+<!-- older smartphones support -->
+<meta name="handheldfriendly" content="true" />
+<meta name="mobileoptimized" content="240" />
+<!-- /older smartphones support -->
+
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+<link rel="stylesheet" lang="text/css" href="css/color.css" />
+<link rel="stylesheet" lang="text/css" href="css/layout.css" />
+<link rel="stylesheet" lang="text/css" href="css/text.css" />
